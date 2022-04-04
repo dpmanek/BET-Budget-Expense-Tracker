62a603b3a2306773cbacf9d2090f01e2ea86407b
dpmanek <dpmanek@gmail.com>
2022-04-04 16:42:19 -0400

    created piechart component for expense

---
+++ Frontend/budget_tracker/src/Components/Graphs/piechart.jsx
@@ -0,0 +1,79 @@
+import React from 'react'
+//import { render } from 'react-dom'
+import Highcharts from 'highcharts'
+import HighchartsReact from 'highcharts-react-official'
+
+const options = {
+//   title: {
+//     text: 'My chart'
+//   },
+//   series: [{
+//     data: [1, 2, 3]
+//   }]
+
+    chart: {
+      plotBackgroundColor: null,
+      plotBorderWidth: null,
+      plotShadow: false,
+      type: 'pie'
+    },
+    title: {
+      text: 'Expense Structure'
+    },
+    tooltip: {
+      pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    accessibility: {
+      point: {
+        valueSuffix: '%'
+      }
+    },
+    plotOptions: {
+      pie: {
+        allowPointSelect: true,
+        cursor: 'pointer',
+        dataLabels: {
+          enabled: true,
+          format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+        }
+      }
+    },
+    series: [{
+      name: 'Party',
+      colorByPoint: true,
+      data: [{
+        name: '',
+        y: 61.41,
+        sliced: true,
+        selected: true
+      }, {
+        name: 'Food & Drinks',
+        y: 11.84
+      }, {
+        name: 'Transportation',
+        y: 10.85
+      }, {
+        name: 'Shopping',
+        y: 4.67
+      }, {
+        name: 'Communication,PC',
+        y: 4.18
+      }, {
+        name: 'Vehicle',
+        y: 1.64
+      }, {
+        name: 'Others',
+        y: 1.6
+      }]
+    }]
+  
+}
+
+const Pie = () => <div>
+  <HighchartsReact
+    highcharts={Highcharts}
+    options={options}
+  />
+</div>
+
+export default Pie;
