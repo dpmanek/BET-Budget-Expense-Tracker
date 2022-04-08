f139be888e2a905ea30589801b8d285110142d0e
Kevin dsa <67777018+kevin46dsa@users.noreply.github.com>
2022-04-08 16:53:03 -0400

    Strarted boilerplate of Login

--- Frontend/budget_tracker/src/routes/routes.js
+++ Frontend/budget_tracker/src/routes/routes.js
@@ -1,11 +1,13 @@
 import { Routes as Switch, Route as Routing } from "react-router-dom";
 import Home from '../Components/Home/Home'
+import Login from '../Components/login/login';
 
 const Routesr =()=>{
     return(
         <>
         <Switch>
         <Routing exact path="/" element={<Home/>} />
+        <Routing exact path="/login" element={<Login/>}/>
         </Switch>
         </>
     )
