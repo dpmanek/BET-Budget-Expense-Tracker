0acadcdf103394a9d75c38ab3dc6e07d2f3c6401
dpmanek <dpmanek@gmail.com>
2022-04-11 14:44:29 -0400

    adding express server and routes

--- Frontend/budget_tracker/src/App.js
+++ Frontend/budget_tracker/src/App.js
@@ -1,16 +1,13 @@
-//import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter } from "react-router-dom";
-import Routesr from './routes/routes'
-// import Pie from './Components/Graphs/piechart'
-// import BarChart from './Components/Graphs/barChart'
+import { BrowserRouter } from 'react-router-dom';
+import Routesr from './routes/routes';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routesr />
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<Routesr />
+		</BrowserRouter>
+	);
 }
 
 export default App;
--- Frontend/budget_tracker/src/Components/Home/Home.jsx
+++ Frontend/budget_tracker/src/Components/Home/Home.jsx
@@ -1,15 +1,13 @@
-//import logo from './logo.svg';
-//-import './App.css';
-import Pie from '../Graphs/piechart'
-import BarChart from '../Graphs/barChart'
+import Pie from '../Graphs/piechart';
+import BarChart from '../Graphs/barChart';
 
-const Home =() =>{
-  return (
-    <div className="App">
-<Pie/>
-<BarChart/>
-    </div>
-  );
-}
+const Home = () => {
+	return (
+		<div className="App">
+			<Pie />
+			<BarChart />
+		</div>
+	);
+};
 
 export default Home;
