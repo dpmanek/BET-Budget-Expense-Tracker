b706930353a84b7cd44c42781861bb5018c1614b
Kevin dsa <67777018+kevin46dsa@users.noreply.github.com>
2022-04-14 01:20:20 -0400

    Added Express, Logging MiddleWare to app . js

--- server/app.js
+++ server/app.js
@@ -9,8 +9,28 @@ const port = 8080;
 app.use('/public', static);
 app.use(express.urlencoded({ extended: true }));
 
+
+
+
+// logging middle ware
+app.use(async(req,res,next)=>{
+    let currentTime = new Date().toUTCString();
+    let method = req.method;
+    let route = req.originalUrl;
+
+    //let authenticated = undefined;
+    // Un Comment this part once Sessions are done
+    //if (req.session.user) authenticated = true;
+    //else authenticated = false; 
+
+    //console.log(`Time: ${currentTime}, Method: ${method}, Route: ${route}, userAuth Status: ${authenticated}`) 
+    console.log(`Time: ${currentTime}, Method: ${method}, Route: ${route}`);
+	next();
+} )
+
 configRoutes(app);
 
-app.listen(port, () => {
-	console.log(`Your server is running on port ${port}`);
-});
+app.listen(8080, () => {
+	console.log("We've now got a server!");
+	console.log('Your routes will be running on http://localhost:3000');
+  });
