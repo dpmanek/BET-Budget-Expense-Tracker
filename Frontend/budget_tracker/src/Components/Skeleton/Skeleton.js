171d90b582c05b112a41b5005a9dd87683d6245e
niravjain98 <niravjain98@gmail.com>
2022-04-16 13:38:41 -0400

    Fixed CSS Bug

---
+++ Frontend/budget_tracker/src/Components/Skeleton/Skeleton.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from '../Navbar/Navbar'
+import Footer from '../Footer/Footer'
+
+const Skeleton = () => {
+  return (
+    <div>
+
+    <Navbar />
+  <h2>Some Content</h2>
+    <Footer/>
+    </div>
+  )
+}
+
+export default Skeleton
