895c729bac12ea7763ae85cfe43f99df0ade1592
dpmanek <dpmanek@gmail.com>
2022-05-03 16:54:29 -0400

    fixed homepage bug

--- Frontend/budget_tracker/src/Components/Banner/Banner.js
+++ Frontend/budget_tracker/src/Components/Banner/Banner.js
@@ -1,65 +1,42 @@
-import React from "react";
-import { Link, useNavigate } from "react-router-dom";
-import "./Banner.css";
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import './Banner.css';
 
 const Banner = (props) => {
-  let navigate = useNavigate();
-  const redirectRoute = (path) => {
-    navigate(path);
-  };
-  return (
-    <div className="div-class">
-      {/* <section className="flex-banner">
-        <figure>
-          <div className="image">
-            <img className="image-div" src="/capture.png" />
-          </div>
-        </figure>
-        <figcaption className="text">
-          <h1>{props.title}</h1>
-          <p>{props.description}</p>
-          <button
-            className="btn btn-outline-success"
-            type="button"
-            onClick={() => redirectRoute("/signup")}
-          >
-            Sign up for more
-          </button>
-        </figcaption>
-      </section> */}
-      <div className="row col-md-12 page">
-        <div className="col-md-6">
-          <img src="/capture.png" className="card-img-top" />
-        </div>
+	let navigate = useNavigate();
+	const redirectRoute = (path) => {
+		navigate(path);
+	};
 
-        <div className="col-md-6" id="center">
-          <h5 className="card-title">{props.title}</h5>
-          <p className="card-text">{props.description}</p>
-          {/* <a href="#" className="btn btn-primary">
-            Go somewhere
-          </a> */}
-        </div>
-        
-      </div>
-
-      <div className="div-class">
-      <div className="row col-md-12 page">
-        
-
-        <div className="col-md-6" id="center">
-          <h5 className="card-title">{props.title}</h5>
-          <p className="card-text">{props.description}</p>
-        </div>
-        
-        <div className="col-md-6">
-          <img src="/capture.png" className="card-img-top" />
-        </div>
-      </div>
-    </div>
-
-    </div>
-    
-  );
+	if (props.type) {
+		return (
+			<div className="div-class">
+				<div className="row col-md-12 page">
+					<div className="col-md-6">
+						<img src="/capture.png" className="card-img-top" />
+					</div>
+					<div className="col-md-6" id="center">
+						<h5 className="card-title">{props.title}</h5>
+						<p className="card-text">{props.description}</p>
+					</div>
+				</div>
+			</div>
+		);
+	} else {
+		return (
+			<div className="div-class">
+				<div className="row col-md-12 page">
+					<div className="col-md-6" id="center">
+						<h5 className="card-title">{props.title}</h5>
+						<p className="card-text">{props.description}</p>
+					</div>
+					<div className="col-md-6">
+						<img src="/capture.png" className="card-img-top" />
+					</div>
+				</div>
+			</div>
+		);
+	}
 };
 
 export default Banner;
--- Frontend/budget_tracker/src/Components/Content/Content.js
+++ Frontend/budget_tracker/src/Components/Content/Content.js
@@ -1,48 +1,63 @@
-import React from "react";
+import React from 'react';
 // import "../Content/Content.css";
 // import { useNavigate } from "react-router-dom";
-import Banner from "../Banner/Banner";
+import Banner from '../Banner/Banner';
 
 const Content = () => {
-  let loop = [
-    { id:0,
-      title: "Always know what's in your pocket",
-      description:
-        "We crunch the numbers to show how much spendable money you have after setting aside enough for bills, goals, and necessities.",
-        // url: !url?"/public/static/no_image.jpeg":url
-    },
-    {id:1,
-      title: "Keep tabs on your spending",
-      description:
-        "See which expenses eat up too much of the pie. Personalize your reports with custom categories and descriptions.",
-    },
-    {id:2,
-      title: "See all accounts in one place",
-      description:
-        "Get a centralized view of all your accounts in one place, without linking your actual bank accounts.",
-    },
-    {id:3,
-      title: "Financial statement",
-      description:
-        "Get a customized statement that would list all the expenses and income from that period.",
-    },
-    {id:4,
-      title: "Monthly expense comparison",
-      description:
-        "Get a category-wise monthly comparison to get a track of your spending and save accordingly for the future.",
-    },
-  ];
-  // let navigate = useNavigate();
-  // const redirectRoute = (path) => {
-  //   navigate(path);
-  // };
-  return (
-    <React.Fragment>
-      {loop.map((i) => {
-        return <Banner title={i.title} description={i.description} />;
-      })}
-    </React.Fragment>
-  );
+	let loop = [
+		{
+			id: 0,
+			title: "Always know what's in your pocket",
+			description:
+				'We crunch the numbers to show how much spendable money you have after setting aside enough for bills, goals, and necessities.',
+			// url: !url?"/public/static/no_image.jpeg":url
+		},
+		{
+			id: 1,
+			title: 'Keep tabs on your spending',
+			description:
+				'See which expenses eat up too much of the pie. Personalize your reports with custom categories and descriptions.',
+		},
+		{
+			id: 2,
+			title: 'See all accounts in one place',
+			description:
+				'Get a centralized view of all your accounts in one place, without linking your actual bank accounts.',
+		},
+		{
+			id: 3,
+			title: 'Financial statement',
+			description:
+				'Get a customized statement that would list all the expenses and income from that period.',
+		},
+		{
+			id: 4,
+			title: 'Monthly expense comparison',
+			description:
+				'Get a category-wise monthly comparison to get a track of your spending and save accordingly for the future.',
+		},
+	];
+	return (
+		<>
+			<Banner
+				title={loop[0].title}
+				description={loop[0].description}
+				type="right"
+			/>
+			<Banner title={loop[1].title} description={loop[1].description} />
+			<Banner
+				title={loop[2].title}
+				description={loop[2].description}
+				type="right"
+			/>
+			<Banner title={loop[3].title} description={loop[3].description} />
+			<Banner
+				title={loop[4].title}
+				description={loop[4].description}
+				type="right"
+			/>
+		</>
+	);
 };
 
 export default Content;
