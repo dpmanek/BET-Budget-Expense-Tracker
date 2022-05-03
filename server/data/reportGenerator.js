716025e93da2d9d2de4cfc2017242c1a531bf652
dpmanek <dpmanek@gmail.com>
2022-05-03 16:17:33 -0400

    report generator completed

--- server/data/reportGenerator.js
+++ server/data/reportGenerator.js
@@ -1,43 +1,147 @@
-const PDFGenerator = require('pdfkit')
-const fs = require('fs')
+const fs = require('fs');
+const PDFDocument = require('pdfkit');
 
+function createInvoice(userData, path) {
+	let doc = new PDFDocument({ size: 'A4', margin: 50 });
 
-// instantiate the library
-let theOutput = new PDFGenerator 
+	generateHeader(doc, userData); //pass userData
+	generateTransactionHr(doc);
+	generateInvoiceTable(doc, userData);
+	generateFooter(doc);
 
-// pipe to a writable stream which would save the result into the same directory
-theOutput.pipe(fs.createWriteStream('../public/reports/TestDocument.pdf'))
+	doc.end();
+	doc.pipe(fs.createWriteStream(path));
+}
 
-// theOutput.image('../public/images/logo.png', {
-//     fit: [50, 50],
-//   })
-// .text('Some awesome example text1')
+//function input parameter will be increased to replace userdata
+function generateHeader(doc, userData) {
+	doc
+		.image('../public/images/logo.png', 50, 45, { width: 50 })
+		.fillColor('#000')
+		.fontSize(20)
+		.text('BET', 275, 50, { align: 'right' })
+		.fontSize(10)
+		.text(`Name: ${userData.Name}`, { align: 'right' })
+		.moveDown()
+		.text(`Transactions:`, { align: 'right' })
+		.text(`From: ${userData.From}`, { align: 'right' })
+		.text(`Till: ${userData.Till}`, { align: 'right' });
+}
 
-theOutput
-.image('../public/images/logo.png', 0, 0, { width: 250})
-.fillColor('#000')
-.fontSize(20)
-.text('BET', 275, 50, {align: 'right'})
-.fontSize(10)
-.text(`Name: Deep Manek`, {align: 'right'})
-.moveDown()
-.text(`Transactions:`, {align: 'right'})
-.text(`From: Today`, {align: 'right'})
-.text(`Till: To a certain Date`, {align: 'right'})
+function generateTransactionHr(doc) {
+	generateHr(doc, 185);
 
-//horizontal rule
-const beginningOfPage = 50
-const endOfPage = 550
+	doc.text(`Transactions`, 50, 210).moveDown();
 
-theOutput.moveTo(beginningOfPage,200)
-    .lineTo(endOfPage,200)
-    .stroke()
+	generateHr(doc, 252);
+}
 
-theOutput.text(`Transactions`, 50, 210)
+function generateInvoiceTable(doc, userData) {
+	let i;
+	const invoiceTableTop = 330;
 
-theOutput.moveTo(beginningOfPage,250)
-    .lineTo(endOfPage,250)
-    .stroke()
+	doc.font('Helvetica-Bold');
+	generateTableRow(
+		doc,
+		invoiceTableTop,
+		'Sr. no',
+		'Transaction Name',
+		'Amount',
+		'Type',
+		'Date'
+	);
+	generateHr(doc, invoiceTableTop + 20);
+	doc.font('Helvetica');
+	//invoice.items=userData.Transactions
+	for (i = 0; i < userData.Transactions.length; i++) {
+		const item = userData.Transactions[i];
+		const position = invoiceTableTop + (i + 1) * 30;
+		generateTableRow(
+			doc,
+			position,
+			item.Sr_no,
+			item.Transaction_Name,
+			formatCurrency(item.Amount),
+			item.Type,
+			item.Date
+		);
 
-// write out file
-theOutput.end()
+		generateHr(doc, position + 20);
+	}
+	doc.font('Helvetica');
+}
+
+function generateFooter(doc) {
+	doc
+		.fontSize(10)
+		.text('Thanks for using BET.', 50, 780, { align: 'center', width: 500 });
+}
+
+function generateTableRow(
+	doc,
+	y,
+	item,
+	description,
+	unitCost,
+	quantity,
+	lineTotal
+) {
+	doc
+		.fontSize(10)
+		.text(item, 50, y)
+		.text(description, 150, y)
+		.text(unitCost, 280, y, { width: 90, align: 'right' })
+		.text(quantity, 370, y, { width: 90, align: 'right' })
+		.text(lineTotal, 0, y, { align: 'right' });
+}
+
+function generateHr(doc, y) {
+	doc.strokeColor('#aaaaaa').lineWidth(1).moveTo(50, y).lineTo(550, y).stroke();
+}
+
+function formatCurrency(Amount) {
+	return '$' + Amount;
+}
+
+module.exports = {
+	createInvoice,
+};
+
+const userdata = {
+	Name: 'Deep Manek',
+	From: '02-05-2022',
+	Till: '03-05-2022',
+	Transactions: [
+		{
+			Sr_no: 1,
+			Transaction_Name: 'Gas',
+			Amount: 23,
+			Type: 'Debit',
+			Date: '02-03-2022',
+		},
+		{
+			Sr_no: 2,
+			Transaction_Name: 'Zel',
+			Amount: 100,
+			Type: 'Credit',
+			Date: '02-04-2022',
+		},
+		{
+			Sr_no: 3,
+			Transaction_Name: 'Pizza',
+			Amount: 7.5,
+			Type: 'Debit',
+			Date: '02-05-2022',
+		},
+		{
+			Sr_no: 4,
+			Transaction_Name: 'Stevens',
+			Amount: 60000,
+			Type: 'Debit',
+			Date: '02-08-2022',
+		},
+	],
+};
+const Path = 'report.pdf';
+
+createInvoice(userdata, Path);
