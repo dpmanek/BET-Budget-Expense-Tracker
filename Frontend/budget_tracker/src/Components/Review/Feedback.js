5df6c683f163dc0bee29f32e1e19eeef806628aa
niravjain98 <niravjain98@gmail.com>
2022-05-04 18:31:58 -0400

    Sloving Merge conflict

--- Frontend/budget_tracker/src/Components/Review/Feedback.js
+++ Frontend/budget_tracker/src/Components/Review/Feedback.js
@@ -4,51 +4,27 @@ import reviewService from "../../services/review.service";
 
 const Feedback = () => {
   const [feedback, setfeedback] = useState("");
-  
+
   useEffect(() => {
-<<<<<<< HEAD
-    reviewService.getUserReview().then((response) => {
-      if (response) {
-=======
     reviewService.getAllReview().then((response) => {
-      if(response){
->>>>>>> 22e0809c456d159ee5af159802a3f386c9282321
+      if (response) {
         setfeedback(response.data.Review.feedback);
       } else {
         setfeedback("");
       }
-<<<<<<< HEAD
     });
   }, []);
-=======
-      else{
-        setfeedback("");
-      } })
-    }, []);
-    
-    
->>>>>>> 22e0809c456d159ee5af159802a3f386c9282321
 
   return (
     <div className="feedback-div">
-      <div >
+      <div>
         <div className="mgb-40 padb-30 auto-invert line-b-4 align-center">
-          
-          <h1
-<<<<<<< HEAD
-            className="font-cond-b fg-text-d lts-md fs-300 fs-300-xs no-mg"
-            contenteditable="false"
-          >
-=======
-            className="font-cond-b fg-text-d lts-md"
-            contenteditable="false">
->>>>>>> 22e0809c456d159ee5af159802a3f386c9282321
+          <h1 className="font-cond-b fg-text-d lts-md" contenteditable="false">
             Customer Reviews
           </h1>
         </div>
         <div>
-          <p>
-          Feedback: {feedback}</p>
+          <p>Feedback: {feedback}</p>
         </div>
       </div>
     </div>
