93cf97dd726c486cccd49d66830b2f8bf411cded
dpmanek <dpmanek@gmail.com>
2022-05-07 00:41:12 -0400

    nodemailer and monthly comparision barchart done

--- server/app.js
+++ server/app.js
@@ -34,7 +34,7 @@ app.use('/users', (req, res, next) => {
 
 // verify token
 app.use('/user/data', (req, res, next) => {
-	//let token = req.headers["x-access-token"];
+	let token = req.headers['x-access-token'];
 
 	try {
 		var tokeninBody = req.body.headers['x-access-token'];
@@ -43,8 +43,7 @@ app.use('/user/data', (req, res, next) => {
 	}
 
 	//let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImtldmluQGdtYWlsLmNvbSIsInVzZXJOYW1lIjoia2V2aW4iLCJpYXQiOjE2NTE1OTk4OTksImV4cCI6MTY1MTY4NjI5OX0.5ahbrHoNt9Y2aT4LxeKijqYwgbePqkbJIO5iewYvGkE"
-	let token =
-		'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im5pcmF2amFpbjk4QGdtYWlsLmNvbSIsInVzZXJOYW1lIjoiTmlyYXYiLCJpYXQiOjE2NTE4NzY2NjcsImV4cCI6MTY1MTk2MzA2N30.qaHHrEOiYqgymDag_LtOQ2H_Ow7eTzTJ32SvkIyKOlE';
+	//let token ='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im5pcmF2amFpbjk4QGdtYWlsLmNvbSIsInVzZXJOYW1lIjoiTmlyYXYiLCJpYXQiOjE2NTE4NzY2NjcsImV4cCI6MTY1MTk2MzA2N30.qaHHrEOiYqgymDag_LtOQ2H_Ow7eTzTJ32SvkIyKOlE';
 	if (!token && !tokeninBody) {
 		return res.status(403).send({ message: 'No token provided!' });
 	}
