20cd8560274f9a487bafc2481fc405bf5b86e95b
niravjain98 <niravjain98@gmail.com>
2022-05-08 16:05:25 -0400

    Recurring Bug Resolved

--- Frontend/budget_tracker/src/Components/Dashboard/Settable.js
+++ Frontend/budget_tracker/src/Components/Dashboard/Settable.js
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import UserService from "../../services/user.service";
+import transactionService from "../../services/add.transaction";
+import { Link, useNavigate } from "react-router-dom";
 
 const Settable = () => {
+  let navigate = useNavigate();
+  const [data, setdata] = useState([]);
+  useEffect(() => {
+    UserService.getUserTransactionData().then((response) => {
+      if (response.data) {
+        setdata(response.data.Expenditure.OneTime);
+      }
+    });
+  }, []);
+
   return <div>Settable</div>;
 };
 
