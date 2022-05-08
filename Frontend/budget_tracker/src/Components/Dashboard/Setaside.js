53e1e2dc3a3b94147320ca499f33c2366cfd406e
dpmanek <dpmanek@gmail.com>
2022-05-08 19:36:02 -0400

    creating goal setaside  added

--- Frontend/budget_tracker/src/Components/Dashboard/Setaside.js
+++ Frontend/budget_tracker/src/Components/Dashboard/Setaside.js
@@ -1,143 +1,140 @@
-import React, { useState, Fragment, useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import "./Setaside.css";
-import axios from "axios";
-import AuthService from "../../services/auth.service";
-import transactionService from "../../services/add.transaction";
-import Settable from "./Settable";
+import React, { useState, Fragment, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import './Setaside.css';
+import axios from 'axios';
+import AuthService from '../../services/auth.service';
+import transactionService from '../../services/add.transaction';
+import Settable from './Settable';
 
 const Setaside = () => {
-  let navigate = useNavigate();
+	let navigate = useNavigate();
 
-  //validation
-  const initialValues = {
-    _id: null,
-    goal: "",
-    amount: "",
-  };
-  const [formValues, setFormValues] = useState(initialValues);
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormValues({ ...formValues, [name]: value });
-  };
-  //
+	//validation
+	const initialValues = {
+		_id: null,
+		goal: '',
+		amount: '',
+	};
+	const [formValues, setFormValues] = useState(initialValues);
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		setFormValues({ ...formValues, [name]: value });
+	};
+	//
 
-  const [error, setError] = useState(null);
-  const [success, setSuccess] = useState("");
-  const [formErrors, setFormErrors] = useState({});
+	const [error, setError] = useState(null);
+	const [success, setSuccess] = useState('');
+	const [formErrors, setFormErrors] = useState({});
 
-  const [accessToken, setAccessToken] = useState("");
+	const [accessToken, setAccessToken] = useState('');
 
-  useEffect(() => {
-    var data = AuthService.getCurrentUser();
-    if (data) {
-      setAccessToken(data.accessToken);
-    } else {
-      setAccessToken(undefined);
-    }
-  }, []);
+	useEffect(() => {
+		var data = AuthService.getCurrentUser();
+		if (data) {
+			setAccessToken(data.accessToken);
+		} else {
+			setAccessToken(undefined);
+		}
+	}, []);
 
-  const validate = (values) => {
-    const alpha = /^[0-9]+$/;
-    const spec = /^[a-zA-Z0-9!@#\$%\^\&*\)\(+=._-]+$/;
-    const tog = spec.test(values.goal);
-    let errors = {};
-    if (!values.amount) {
-      errors.amount = "Amount is required";
-    }
-    if (!values.goal) {
-      errors.goal = "Goal is required";
-    }
-    if (tog === true) {
-      errors.goal = "Goal cannot be just special characters!";
-    }
-    const flag = alpha.test(values.goal);
-    if (flag === true) {
-      errors.goal = "Goal cannot be just Numerical!";
-    }
-    return errors;
-  };
+	const validate = (values) => {
+		const alpha = /^[0-9]+$/;
 
-  const addGoal = async (event) => {
-    setError("");
-    setSuccess("");
-    event.preventDefault();
-    let error = await validate(formValues);
-    await setFormErrors(error);
-    if (Object.keys(error).length === 0) {
-      let id = formValues._id;
-      transactionService
-        .createSetAside(formValues)
-        .then((data) => {
-          setSuccess("Goal added successfully!!");
-          //   navigate("/dashboard");
-        })
-        .catch((e) => {
-          setError("Opps, something went wrong :(");
-        });
-    }
-  };
+		let errors = {};
+		if (!values.amount) {
+			errors.amount = 'Amount is required';
+		}
+		if (!values.goal) {
+			errors.goal = 'Goal is required';
+		}
 
-  return (
-    <div className="footer-solve">
-      {accessToken !== undefined ? (
-        <React.Fragment>
-          <div className="row col-md-8 offset-md-4">
-            <h1 className="">Set a Goal</h1>
-            <form className="" onSubmit={addGoal}>
-              <div className="mb-3">
-                <label htmlFor="exampleInputEmail1" className="form-label">
-                  Goal
-                </label>
-                <input
-                  id="exampleInputEmail1"
-                  type="text"
-                  name="goal"
-                  placeholder="Goal"
-                  className="form-control position"
-                  value={formValues.goal}
-                  onChange={handleChange}
-                />
-              </div>
-              <p className="disError">{formErrors ? formErrors.goal : ""}</p>
-              <div className="mb-3">
-                <label htmlFor="exampleInputEmail1" className="form-label">
-                  Amount
-                </label>
-                <input
-                  id="exampleInputEmail1"
-                  type="number"
-                  placeholder="Amount"
-                  className="form-control position"
-                  name="amount"
-                  value={formValues.amount}
-                  onChange={handleChange}
-                />
-              </div>
-              <p className="disError">{formErrors ? formErrors.amount : ""}</p>
-              <button type="submit" className="btn btn-primary" value="Submit">
-                Submit
-              </button>
-              <div className="disError">{error !== "" ? error : success}</div>
-            </form>
-          </div>
-          <Settable />
-        </React.Fragment>
-      ) : (
-        <React.Fragment>
-          <div className="card posR posF">
-            <h1>Restricted area</h1>
-            <h2>
-              <a href="/login" className="a12">
-                Sign In
-              </a>{" "}
-              to Set Goals
-            </h2>
-          </div>
-        </React.Fragment>
-      )}
-    </div>
-  );
+		const flag = alpha.test(values.goal);
+		if (flag === true) {
+			errors.goal = 'Goal cannot be just Numerical!';
+		}
+		return errors;
+	};
+
+	const addGoal = async (event) => {
+		setError('');
+		setSuccess('');
+		event.preventDefault();
+		let error = await validate(formValues);
+		await setFormErrors(error);
+		if (Object.keys(error).length === 0) {
+			let id = formValues._id;
+			transactionService
+				.createSetAside(formValues)
+				.then((data) => {
+					setSuccess('Goal added successfully!!');
+					//   navigate("/dashboard");
+				})
+				.catch((e) => {
+					setError('Opps, something went wrong :(');
+				});
+		}
+	};
+
+	return (
+		<div className="footer-solve">
+			{accessToken !== undefined ? (
+				<React.Fragment>
+					<div className="row col-md-8 offset-md-4">
+						<h1 className="">Set a Goal</h1>
+						<form className="" onSubmit={addGoal}>
+							<div className="mb-3">
+								<label htmlFor="exampleInputEmail1" className="form-label">
+									Goal
+								</label>
+								<input
+									id="exampleInputEmail1"
+									type="text"
+									name="goal"
+									placeholder="Goal"
+									className="form-control position"
+									value={formValues.goal}
+									onChange={handleChange}
+								/>
+							</div>
+							<p className="disError">{formErrors ? formErrors.goal : ''}</p>
+							<div className="mb-3">
+								<label htmlFor="exampleInputEmail1" className="form-label">
+									Amount
+								</label>
+								<input
+									id="exampleInputEmail1"
+									type="number"
+									placeholder="Amount"
+									className="form-control position"
+									name="amount"
+									value={formValues.amount}
+									onChange={handleChange}
+								/>
+							</div>
+							<p className="disError">{formErrors ? formErrors.amount : ''}</p>
+							<button type="submit" className="btn btn-primary" value="Submit">
+								Submit
+							</button>
+							<div className="disError">{error !== '' ? error : success}</div>
+						</form>
+					</div>
+					<Settable />
+				</React.Fragment>
+			) : (
+				<React.Fragment>
+					<div className="card posR posF">
+						<h1>Restricted area</h1>
+						<h2>
+							<a href="/login" className="a12">
+								Sign In
+							</a>{' '}
+							to Set Goals
+						</h2>
+					</div>
+				</React.Fragment>
+			)}
+		</div>
+	);
 };
 
 export default Setaside;
