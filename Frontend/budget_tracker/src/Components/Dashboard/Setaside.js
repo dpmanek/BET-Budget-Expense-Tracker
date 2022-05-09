12129705ac659483ccbc55c2977a590da3508906
dpmanek <dpmanek@gmail.com>
2022-05-08 22:07:51 -0400

    resolved all warnings

--- Frontend/budget_tracker/src/Components/Dashboard/Setaside.js
+++ Frontend/budget_tracker/src/Components/Dashboard/Setaside.js
@@ -1,142 +1,139 @@
-import React, { useState, Fragment, useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import "./Setaside.css";
-import axios from "axios";
-import AuthService from "../../services/auth.service";
-import transactionService from "../../services/add.transaction";
-import Settable from "./Settable";
+import React, { useState, useEffect } from 'react';
+
+import './Setaside.css';
+
+import AuthService from '../../services/auth.service';
+import transactionService from '../../services/add.transaction';
+import Settable from './Settable';
 
 const Setaside = () => {
-  let navigate = useNavigate();
+	//validation
+	const initialValues = {
+		_id: null,
+		goal: '',
+		amount: '',
+	};
+	const [formValues, setFormValues] = useState(initialValues);
+	const [reload, setReload] = useState(false);
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		setFormValues({ ...formValues, [name]: value });
+	};
+	//
 
-  //validation
-  const initialValues = {
-    _id: null,
-    goal: "",
-    amount: "",
-  };
-  const [formValues, setFormValues] = useState(initialValues);
-  const [reload, setReload] = useState(false);
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormValues({ ...formValues, [name]: value });
-  };
-  //
+	const [error, setError] = useState(null);
+	const [success, setSuccess] = useState('');
+	const [formErrors, setFormErrors] = useState({});
 
-  const [error, setError] = useState(null);
-  const [success, setSuccess] = useState("");
-  const [formErrors, setFormErrors] = useState({});
+	const [accessToken, setAccessToken] = useState('');
 
-  const [accessToken, setAccessToken] = useState("");
+	useEffect(() => {
+		var data = AuthService.getCurrentUser();
+		if (data) {
+			setAccessToken(data.accessToken);
+		} else {
+			setAccessToken(undefined);
+		}
+	}, []);
 
-  useEffect(() => {
-    var data = AuthService.getCurrentUser();
-    if (data) {
-      setAccessToken(data.accessToken);
-    } else {
-      setAccessToken(undefined);
-    }
-  }, []);
+	const validate = (values) => {
+		const alpha = /^[0-9]+$/;
 
-  const validate = (values) => {
-    const alpha = /^[0-9]+$/;
+		let errors = {};
+		if (!values.amount) {
+			errors.amount = 'Amount is required';
+		}
+		if (!values.goal) {
+			errors.goal = 'Goal is required';
+		}
 
-    let errors = {};
-    if (!values.amount) {
-      errors.amount = "Amount is required";
-    }
-    if (!values.goal) {
-      errors.goal = "Goal is required";
-    }
+		const flag = alpha.test(values.goal);
+		if (flag === true) {
+			errors.goal = 'Goal cannot be just Numerical!';
+		}
+		return errors;
+	};
 
-    const flag = alpha.test(values.goal);
-    if (flag === true) {
-      errors.goal = "Goal cannot be just Numerical!";
-    }
-    return errors;
-  };
+	const addGoal = async (event) => {
+		setError('');
+		setSuccess('');
+		event.preventDefault();
+		let error = await validate(formValues);
+		await setFormErrors(error);
+		if (Object.keys(error).length === 0) {
+			transactionService
+				.createSetAside(formValues)
+				.then((data) => {
+					setSuccess('Goal added successfully!!');
+					setReload(true);
+					//   navigate("/dashboard");
+				})
+				.catch((e) => {
+					setError('Opps, something went wrong :(');
+				});
+		}
+	};
 
-  const addGoal = async (event) => {
-    setError("");
-    setSuccess("");
-    event.preventDefault();
-    let error = await validate(formValues);
-    await setFormErrors(error);
-    if (Object.keys(error).length === 0) {
-      let id = formValues._id;
-      transactionService
-        .createSetAside(formValues)
-        .then((data) => {
-          setSuccess("Goal added successfully!!");
-          setReload(true);
-          //   navigate("/dashboard");
-        })
-        .catch((e) => {
-          setError("Opps, something went wrong :(");
-        });
-    }
-  };
-
-  return (
-    <div className="footer-solve">
-      {accessToken !== undefined ? (
-        <React.Fragment>
-          <div className="row col-md-8 offset-md-4">
-            <h1 className="">Set a Goal</h1>
-            <form className="" onSubmit={addGoal}>
-              <div className="mb-3">
-                <label htmlFor="exampleInputEmail1" className="form-label">
-                  Goal
-                </label>
-                <input
-                  id="exampleInputEmail1"
-                  type="text"
-                  name="goal"
-                  placeholder="Goal"
-                  className="form-control position"
-                  value={formValues.goal}
-                  onChange={handleChange}
-                />
-              </div>
-              <p className="disError">{formErrors ? formErrors.goal : ""}</p>
-              <div className="mb-3">
-                <label htmlFor="exampleInputEmail1" className="form-label">
-                  Amount
-                </label>
-                <input
-                  id="exampleInputEmail1"
-                  type="number"
-                  placeholder="Amount"
-                  className="form-control position"
-                  name="amount"
-                  value={formValues.amount}
-                  onChange={handleChange}
-                />
-              </div>
-              <p className="disError">{formErrors ? formErrors.amount : ""}</p>
-              <button type="submit" className="btn btn-primary" value="Submit">
-                Submit
-              </button>
-              <div className="disError">{error !== "" ? error : success}</div>
-            </form>
-          </div>
-          <Settable reload={reload} />
-        </React.Fragment>
-      ) : (
-        <React.Fragment>
-          <div className="card posR posF">
-            <h1>Restricted area</h1>
-            <h2>
-              <a href="/login" className="a12">
-                Sign In
-              </a>{" "}
-              to Set Goals
-            </h2>
-          </div>
-        </React.Fragment>
-      )}
-    </div>
-  );
+	return (
+		<div className="footer-solve">
+			{accessToken !== undefined ? (
+				<React.Fragment>
+					<div className="row col-md-8 offset-md-4">
+						<h1 className="">Set a Goal</h1>
+						<form className="" onSubmit={addGoal}>
+							<div className="mb-3">
+								<label htmlFor="exampleInputEmail1" className="form-label">
+									Goal
+								</label>
+								<input
+									id="exampleInputEmail1"
+									type="text"
+									name="goal"
+									placeholder="Goal"
+									className="form-control position"
+									value={formValues.goal}
+									onChange={handleChange}
+								/>
+							</div>
+							<p className="disError">{formErrors ? formErrors.goal : ''}</p>
+							<div className="mb-3">
+								<label htmlFor="exampleInputEmail1" className="form-label">
+									Amount
+								</label>
+								<input
+									id="exampleInputEmail1"
+									type="number"
+									placeholder="Amount"
+									className="form-control position"
+									name="amount"
+									value={formValues.amount}
+									onChange={handleChange}
+								/>
+							</div>
+							<p className="disError">{formErrors ? formErrors.amount : ''}</p>
+							<button type="submit" className="btn btn-primary" value="Submit">
+								Submit
+							</button>
+							<div className="disError">{error !== '' ? error : success}</div>
+						</form>
+					</div>
+					<Settable reload={reload} />
+				</React.Fragment>
+			) : (
+				<React.Fragment>
+					<div className="card posR posF">
+						<h1>Restricted area</h1>
+						<h2>
+							<a href="/login" className="a12">
+								Sign In
+							</a>{' '}
+							to Set Goals
+						</h2>
+					</div>
+				</React.Fragment>
+			)}
+		</div>
+	);
 };
 
 export default Setaside;
--- Frontend/budget_tracker/src/Components/Dashboard/Settable.js
+++ Frontend/budget_tracker/src/Components/Dashboard/Settable.js
@@ -1,66 +1,64 @@
-import React, { useState, useEffect } from "react";
-import UserService from "../../services/user.service";
-import transactionService from "../../services/add.transaction";
-import { useNavigate } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+
+import transactionService from '../../services/add.transaction';
 
 const Settable = (props) => {
-  let navigate = useNavigate();
-  const [data, setdata] = useState([]);
-  useEffect(() => {
-    transactionService.getSetAside().then((response) => {
-      if (response.data) {
-        setdata(response.data);
-      }
-    });
-  }, [props]);
+	const [data, setdata] = useState([]);
+	useEffect(() => {
+		transactionService.getSetAside().then((response) => {
+			if (response.data) {
+				setdata(response.data);
+			}
+		});
+	}, [props]);
 
-  const handleDelete = (event) => {
-    event.preventDefault();
-    let id = event.target.value;
-    transactionService
-      .deleteSetAside(id)
-      .then((d) => {
-        let tmpData = data.filter((d) => d._id != id);
-        setdata(tmpData);
-      })
-      .catch((e) => {
-        console.log("Error in deleting Goals");
-      });
-  };
+	const handleDelete = (event) => {
+		event.preventDefault();
+		let id = event.target.value;
+		transactionService
+			.deleteSetAside(id)
+			.then((d) => {
+				let tmpData = data.filter((d) => d._id !== id);
+				setdata(tmpData);
+			})
+			.catch((e) => {
+				console.log('Error in deleting Goals');
+			});
+	};
 
-  return (
-    <div>
-      <table className="table table-border">
-        <thead>
-          <tr>
-            <th>Goal</th>
-            <th>Amount</th>
-            <th>Delete</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((d) => {
-            return (
-              <tr>
-                <td>{d.Purpose}</td>
-                <td>${d.Amount}</td>
+	return (
+		<div>
+			<table className="table table-border">
+				<thead>
+					<tr>
+						<th>Goal</th>
+						<th>Amount</th>
+						<th>Delete</th>
+					</tr>
+				</thead>
+				<tbody>
+					{data.map((d) => {
+						return (
+							<tr>
+								<td>{d.Purpose}</td>
+								<td>${d.Amount}</td>
 
-                <td>
-                  <button
-                    className="btn btn-danger"
-                    value={d._id}
-                    onClick={handleDelete}
-                  >
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </div>
-  );
+								<td>
+									<button
+										className="btn btn-danger"
+										value={d._id}
+										onClick={handleDelete}
+									>
+										Delete
+									</button>
+								</td>
+							</tr>
+						);
+					})}
+				</tbody>
+			</table>
+		</div>
+	);
 };
 
 export default Settable;
